23cb06631471d1ce592b3d0e58025935422cd704
miazocool <zaimsaharudin@gmail.com>
2022-04-10 17:18:16 +0800

    Does it work

--- pairs_trading_cryptocurrencies_strategy_catalyst.py
+++ pairs_trading_cryptocurrencies_strategy_catalyst.py
@@ -6,7 +6,7 @@ import scipy.stats as st
 from catalyst import run_algorithm
 from catalyst.api import (record, symbol, order_target_percent, date_rules, time_rules, get_datetime)
 
-#zaim from miazocool
+#zaim from miazocool does it work
 def initialize(context):
     context.A = symbol('xmr_usd')
     context.B = symbol('neo_usd')
